31e16fa3cda8e43bc6526df425817b466cea6a90
Abhijith-Anil <03abhijithanil@gmail.com>
2024-03-03 09:20:57 +0530

    newcodeadded

--- products/views.py
+++ products/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def list_products(request):
+   """_summary_
+   return product list page
+   Args:
+   request (_type_): _description_
+
+   Returns:
+  _type_:_description_
+ """
+   product_list=Product.objects.all()
+   context={'products':product_list}
+   return render(request,'products.html',context)
+
+
+def detail_products(request):
+    return render(request,'product_detail.html')
